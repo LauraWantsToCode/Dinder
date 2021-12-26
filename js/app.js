d401c165b463dddd3ea236d25cbf3279c768192c
LauraWantsToCode <murzinaite@gmail.com>
2021-12-26 21:30:25 +0000

    fifth

--- js/app.js
+++ js/app.js
@@ -42,3 +42,13 @@ function Hide3() {
 function IMGsrc(source) {
     document.getElementById('img-preview').src = source;
   }
+
+const BtnUp = document.getElementById("BtnUp").innerHTML; 
+
+BtnUp.AddEventListener('click', function() {
+    // when using function in {} instead of =>, to ensure IE compatibility
+window.scrollTo(0, 0); 
+// above 0 means pixels. first - X (left), second - y (top)
+// adding 50 would mean 50px from the top
+})
+
